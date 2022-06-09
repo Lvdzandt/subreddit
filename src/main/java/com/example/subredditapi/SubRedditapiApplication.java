8c86955b06d58d2851c00308f0ca06f962475c63
Lvdzandt <lucvdzandt@gmail.com>
2022-06-09 11:30:12 +0200

    added kafka

--- src/main/java/com/example/subredditapi/SubRedditapiApplication.java
+++ src/main/java/com/example/subredditapi/SubRedditapiApplication.java
@@ -1,16 +1,17 @@
 package com.example.subredditapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.KafkaTemplate;
 
 
 @EntityScan(basePackages = {"com.example.subredditapi"})
 @EnableJpaRepositories(basePackages = {"com.*"})
 @SpringBootApplication
-@EnableKafka
 public class SubRedditapiApplication {
 
     public static void main(String[] args) {
