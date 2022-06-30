979f3552bdf47cd3c3da652724311132d0e5701e
Lvdzandt <lucvdzandt@gmail.com>
2022-06-30 11:27:20 +0200

    added cors

--- src/main/java/com/example/subredditapi/CorsConfig.java
+++ src/main/java/com/example/subredditapi/CorsConfig.java
@@ -2,27 +2,35 @@ package com.example.subredditapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-@Configuration
-public class CorsConfig {
-    @Bean
-    public CorsWebFilter corsWebFilter() {
 
-        final CorsConfiguration corsConfig = new CorsConfiguration ();
-        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE"));
-        corsConfig.addAllowedHeader("*");
+@EnableWebSecurity
+public class CorsConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
 
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+        // You can customize the following part based on your project, it's only a sample
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
 
-        return new CorsWebFilter(source);
     }
 }
